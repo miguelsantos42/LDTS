761df45dd9cb258fabc1fbc59d5426e148034036
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2022-11-15 12:09:12 +0000

    Player and Position Classes and Tests Files Created.

--- src/test/java/ObjectsTest/PositionTest.java
+++ src/test/java/ObjectsTest/PositionTest.java
@@ -1,4 +1,41 @@
 package ObjectsTest;
 
+import Objects.Position;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 public class PositionTest {
+
+    Position position;
+
+    @ParameterizedTest
+    @MethodSource("streamPositionConstructor")
+    public void testXPositonConstructor(int x, int y){
+        this.position = new Position(x, y);
+        assertEquals(x, this.position.getxPos());
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamPositionConstructor")
+    public void testYPositonConstructor(int x, int y){
+        this.position = new Position(x, y);
+        assertEquals(y, this.position.getyPos());
+    }
+
+
+    private static Stream<Arguments> streamPositionConstructor() {
+        return Stream.of(
+                arguments(1, 2),
+                arguments(2, 2),
+                arguments(10, 15)
+        );
+    }
 }
