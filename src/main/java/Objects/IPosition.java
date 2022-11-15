ec5301c7be7f3e0bc28f29e192a502f6cbb5572a
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2022-11-15 11:36:54 +0000

    IPostion Interface

---
+++ src/main/java/Objects/IPosition.java
@@ -0,0 +1,11 @@
+package Objects;
+
+public interface IPosition {
+
+    public int getxPos();
+    public int getyPos();
+
+    public void setxPos(int xPos);
+    public void setyPos(int yPos);
+
+}
