a734085d4f13408fa5c9ba7102acc9651722174a
Matias Vaz <matiasfreitasguimaraes@gmail.com>
2022-11-15 12:09:12 +0000

    Player and Position Classes and Tests Files Created.

--- src/main/java/Objects/IGameObject.java
+++ src/main/java/Objects/IGameObject.java
@@ -2,10 +2,10 @@ package Objects;
 
 public interface IGameObject {
 
-    public void moveRight(int x);
-    public void moveLeft(int x);
-    public void moveUp(int y);
-    public void moveDown(int y);
+    public void moveRight(int xBias);
+    public void moveLeft(int xBias);
+    public void moveUp(int yBias);
+    public void moveDown(int yBias);
 
     public void doAction(int command);
 
--- src/main/java/Objects/Position.java
+++ src/main/java/Objects/Position.java
@@ -1,4 +1,32 @@
 package Objects;
 
-public class Position {
+public class Position implements IPosition{
+
+    int x;
+    int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int getxPos() {
+        return x;
+    }
+
+    @Override
+    public int getyPos() {
+        return x;
+    }
+
+    @Override
+    public void setxPos(int xPos) {
+        x = x - xPos;
+    }
+
+    @Override
+    public void setyPos(int yPos) {
+        y = y - yPos;
+    }
 }
