ccd87c6bbe914e134ee9a8a03434d8df4eac36b7
Matias FG <matiasfreitasguimaraes@gmail.com>
2022-12-12 13:43:17 +0000

    New Bullet Tests

--- src/test/java/ObjectsTest/BulletTest.java
+++ src/test/java/ObjectsTest/BulletTest.java
@@ -18,7 +18,7 @@ public class BulletTest {
 
     @ParameterizedTest
     @MethodSource("streamDownMoveLimit")
-    public void testDownPlayerMoveLimit(Player player, int height){
+    public void testDownPlayerMoveLimit(Player player, int height) {
         int currentPosition = player.getPosition().getyPos();
         player.moveDown();
         assertEquals(player.getPosition().getyPos(), currentPosition);
@@ -26,15 +26,15 @@ public class BulletTest {
 
 
     private static Stream<Arguments> streamDownMoveLimit() {
-        Player gameObject1 = new Player(new Position(1, 2), new Size(1,2), new TextColor.RGB(255, 255, 255), new Life(1));
-        Player gameObject2 = new Player(new Position(2, 1), new Size(3,20), new TextColor.RGB(255, 255, 255), new Life(1));
-        Player gameObject3 = new Player(new Position(3, 1), new Size(10,20), new TextColor.RGB(255, 255, 255), new Life(1));
+        Player gameObject1 = new Player(new Position(1, 2), new Size(1, 2), new TextColor.RGB(255, 255, 255), new Life(1));
+        Player gameObject2 = new Player(new Position(2, 1), new Size(3, 20), new TextColor.RGB(255, 255, 255), new Life(1));
+        Player gameObject3 = new Player(new Position(3, 1), new Size(10, 20), new TextColor.RGB(255, 255, 255), new Life(1));
 
 
         return Stream.of(
-                arguments(gameObject1,  2),
-                arguments(gameObject2,  4),
-                arguments(gameObject3,  101)
+                arguments(gameObject1, 2),
+                arguments(gameObject2, 4),
+                arguments(gameObject3, 101)
         );
     }
-
+}
